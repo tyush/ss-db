ca7e1a3757bbcf34032fe3b929bc77e0c842850f
tyush <tyushk@gmail.com>
2022-03-23 20:28:22 -0500

    Add generic "all responses" endpoints

--- src/paths/api.rs
+++ src/paths/api.rs
@@ -29,10 +29,39 @@ async fn get_all_by_team_pit(
     Ok(HttpResponse::Ok().json(responses))
 }
 
+#[get("/api/2022/all/pit")]
+async fn get_all_pit(
+    _req: HttpRequest,
+    data: web::Data<AppState>
+) -> WebResult {
+    use entities::pit_responses_2022 as pit_resp;
+
+    let responses: Vec<pit_resp::Model> = pit_resp::Entity::find()
+        .all(&data.conn)
+        .await?;
+
+    Ok(HttpResponse::Ok().json(responses))
+}
+
 #[get("/api/2022/all/match/{team}")]
 async fn get_all_by_team_match(
     _req: HttpRequest,
     path: web::Path<(u32,)>,
+    data: web::Data<AppState>
+) -> WebResult {
+    use entities::match_responses_2022 as match_resp;
+
+    let responses: Vec<match_resp::Model> = match_resp::Entity::find()
+        .filter(match_resp::Column::TeamNumber.eq(path.0))
+        .all(&data.conn)
+        .await?;
+
+    Ok(HttpResponse::Ok().json(responses))
+}
+
+#[get("/api/2022/all/match")]
+async fn get_all_match(
+    _req: HttpRequest,
     data: web::Data<AppState>
 ) -> WebResult {
     use entities::match_responses_2022 as match_resp;
